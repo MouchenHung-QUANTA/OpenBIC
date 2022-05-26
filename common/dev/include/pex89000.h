d54537a9c388c209d5e981182420717ced9eae96
MouchenHung <Mouchen.Hung@quantatw.com>
2022-05-26 17:13:29 +0800

    commit-20220526-First commit

---
+++ common/dev/include/pex89000.h
@@ -0,0 +1,26 @@
+#ifndef PEX_89000_H
+#define PEX_89000_H
+
+#include <stdint.h>
+
+typedef enum pex_dev { pex_dev_atlas1, pex_dev_atlas2, pex_dev_unknown } pex_dev_t;
+
+typedef enum pex_access {
+	pex_access_temp,
+	pex_access_adc,
+	pex_access_id,
+	pex_access_rev_id,
+	pex_access_sbr_ver,
+	pex_access_flash_ver,
+	pex_access_unknown
+} pex_access_t;
+
+/* sensor offset */
+enum pex_sensor_offset {
+	PEX_TEMP,
+	PEX_ADC,
+};
+
+uint8_t pex_access_engine(uint8_t bus, uint8_t addr, pex_access_t key, uint32_t *resp);
+
+#endif
