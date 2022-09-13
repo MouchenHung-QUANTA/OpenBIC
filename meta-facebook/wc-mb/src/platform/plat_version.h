d30fd5aa2aad64d84fd7174b17fc81d0c06bbd21
MouchenHung <Mouchen.Hung@quantatw.com>
2022-09-13 16:48:38 +0800

    switch version

--- meta-facebook/wc-mb/src/platform/plat_version.h
+++ meta-facebook/wc-mb/src/platform/plat_version.h
@@ -15,7 +15,7 @@
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0x00 // Count of release firmware at each stage.
+#define FIRMWARE_REVISION_2 0x04 // Count of release firmware at each stage.
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -24,8 +24,8 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x22
-#define BIC_FW_WEEK 0x00
-#define BIC_FW_VER 0x00
+#define BIC_FW_WEEK 0x37
+#define BIC_FW_VER 0x01
 #define BIC_FW_platform_0 0x77 // char: w
 #define BIC_FW_platform_1 0x63 // char: c
 #define BIC_FW_platform_2 0x00 // char: '\0'
