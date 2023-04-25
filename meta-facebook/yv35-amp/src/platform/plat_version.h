5c99ea5fa2b847319b7d0cc3ee2c9cbdf52b8da8
MouchenHung <Mouchen.Hung@quantatw.com>
2023-04-25 09:57:42 +0800

    amp-Modify version

--- meta-facebook/yv35-amp/src/platform/plat_version.h
+++ meta-facebook/yv35-amp/src/platform/plat_version.h
@@ -20,21 +20,22 @@
 #include "version.h"
 
 #define PLATFORM_NAME "Yosemite V3.5"
-#define PROJECT_NAME "Half Dome"
-#define PROJECT_STAGE DVT
+#define PROJECT_NAME "Ampare"
+#define PROJECT_STAGE POC
 
 /*
  * 0x01 Crater Lake
  * 0x02 Baseboard
  * 0x03 Rainbow Falls
  * 0x04 Half Dome
+ * 0x10 Ampere(TODO)
  */
-#define BOARD_ID 0x04
+#define BOARD_ID 0x10
 #define DEVICE_ID 0x00
 #define DEVICE_REVISION 0x80
 
 #define FIRMWARE_REVISION_1 GET_FW_VERSION1(BOARD_ID, PROJECT_STAGE)
-#define FIRMWARE_REVISION_2 0x04
+#define FIRMWARE_REVISION_2 0x01
 
 #define IPMI_VERSION 0x02
 #define ADDITIONAL_DEVICE_SUPPORT 0xBF
@@ -43,10 +44,10 @@
 
 #define BIC_FW_YEAR_MSB 0x20
 #define BIC_FW_YEAR_LSB 0x23
-#define BIC_FW_WEEK 0x11
+#define BIC_FW_WEEK 0x17
 #define BIC_FW_VER 0x01
-#define BIC_FW_platform_0 0x68 // char: h
-#define BIC_FW_platform_1 0x64 // char: d
+#define BIC_FW_platform_0 0x61 // char: a
+#define BIC_FW_platform_1 0x70 // char: p
 #define BIC_FW_platform_2 0x00 // char: '\0'
 
 #endif
