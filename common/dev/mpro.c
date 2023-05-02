8395e5990ecd804514d66cd3a5d257fdbcc94961
MouchenHung <Mouchen.Hung@quantatw.com>
2023-05-02 17:38:30 +0800

    Support Mpro postcode collect

---
+++ common/dev/mpro.c
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <zephyr.h>
+#include <stdlib.h>
+#include "libutil.h"
+#include <logging/log.h>
+#include "ipmi.h"
+#include "ipmb.h"
+
+LOG_MODULE_REGISTER(mpro);
+
+#define MPRO_POSTCODE_LEN 1024
+#define PROCESS_POSTCODE_STACK_SIZE 2048
+
+K_THREAD_STACK_DEFINE(process_postcode_thread, PROCESS_POSTCODE_STACK_SIZE);
+static struct k_thread process_postcode_thread_handler;
+
+static uint32_t mpro_read_buffer[MPRO_POSTCODE_LEN];
+static uint16_t mpro_read_len = 0, mpro_read_index = 0;
+static bool proc_4byte_postcode_ok = false;
+static struct k_sem get_postcode_sem;
+
+uint16_t copy_mpro_read_buffer(uint16_t start, uint16_t length, uint8_t *buffer,
+			       uint16_t buffer_len)
+{
+	if ((buffer == NULL) || (buffer_len < (length * 4))) {
+		return 0;
+	}
+
+	uint16_t current_index, i = 0;
+	uint16_t current_read_len = mpro_read_len;
+	uint16_t current_read_index = mpro_read_index;
+	if (start < current_read_index) {
+		current_index = current_read_index - start - 1;
+	} else {
+		current_index = current_read_index + MPRO_POSTCODE_LEN - start - 1;
+	}
+
+	for (; (i < length) && ((i + start) < current_read_len); i++) {
+		buffer[4 * i] = mpro_read_buffer[current_index] & 0xFF;
+		buffer[(4 * i) + 1] = (mpro_read_buffer[current_index] >> 8) & 0xFF;
+		buffer[(4 * i) + 2] = (mpro_read_buffer[current_index] >> 16) & 0xFF;
+		buffer[(4 * i) + 3] = (mpro_read_buffer[current_index] >> 24) & 0xFF;
+
+		if (current_index == 0) {
+			current_index = MPRO_POSTCODE_LEN - 1;
+		} else {
+			current_index--;
+		}
+	}
+	return 4 * i;
+}
+
+static void process_postcode(void *arvg0, void *arvg1, void *arvg2)
+{
+	uint16_t send_index = 0;
+	while (1) {
+		k_sem_take(&get_postcode_sem, K_FOREVER);
+		ipmi_msg *msg = (ipmi_msg *)malloc(sizeof(ipmi_msg));
+		if (msg == NULL) {
+			LOG_ERR("Memory allocation failed.");
+			continue;
+		}
+
+		uint16_t current_read_index = mpro_read_index;
+		for (; send_index != current_read_index; send_index++) {
+			if (send_index == MPRO_POSTCODE_LEN - 1) {
+				send_index = 0;
+			}
+
+			memset(msg, 0, sizeof(ipmi_msg));
+			msg->InF_source = SELF;
+			msg->InF_target = BMC_IPMB;
+			msg->netfn = NETFN_OEM_1S_REQ;
+			msg->cmd = CMD_OEM_1S_SEND_4BYTE_POST_CODE_TO_BMC;
+			msg->data_len = 8;
+			msg->data[0] = IANA_ID & 0xFF;
+			msg->data[1] = (IANA_ID >> 8) & 0xFF;
+			msg->data[2] = (IANA_ID >> 16) & 0xFF;
+			msg->data[3] = 4;
+			msg->data[4] = mpro_read_buffer[send_index] & 0xFF;
+			msg->data[5] = (mpro_read_buffer[send_index] >> 8) & 0xFF;
+			msg->data[6] = (mpro_read_buffer[send_index] >> 16) & 0xFF;
+			msg->data[7] = (mpro_read_buffer[send_index] >> 24) & 0xFF;
+			ipmb_error status = ipmb_read(msg, IPMB_inf_index_map[msg->InF_target]);
+			if (status != IPMB_ERROR_SUCCESS) {
+				LOG_ERR("Failed to send 4-byte post code to BMC, status %d.",
+					status);
+			}
+			k_yield();
+		}
+		SAFE_FREE(msg)
+	}
+}
+
+void mpro_postcode_insert(uint32_t postcode)
+{
+	proc_4byte_postcode_ok = true;
+
+	mpro_read_buffer[mpro_read_index] = postcode;
+
+	if (mpro_read_len < MPRO_POSTCODE_LEN)
+		mpro_read_len++;
+
+	mpro_read_index++;
+	if (mpro_read_index == MPRO_POSTCODE_LEN)
+		mpro_read_index = 0;
+
+	k_sem_give(&get_postcode_sem);
+}
+
+void mpro_init()
+{
+	k_sem_init(&get_postcode_sem, 0, 1);
+
+	k_thread_create(&process_postcode_thread_handler, process_postcode_thread,
+			K_THREAD_STACK_SIZEOF(process_postcode_thread), process_postcode, NULL,
+			NULL, NULL, CONFIG_MAIN_THREAD_PRIORITY, 0, K_NO_WAIT);
+	k_thread_name_set(&process_postcode_thread_handler, "process_postcode_thread");
+}
+
+void reset_mpro_postcode_buffer()
+{
+	mpro_read_len = 0;
+	return;
+}
+
+bool get_4byte_postcode_ok()
+{
+	return proc_4byte_postcode_ok;
+}
+
+void reset_4byte_postcode_ok()
+{
+	proc_4byte_postcode_ok = false;
+}
