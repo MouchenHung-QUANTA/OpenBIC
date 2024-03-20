9d1da1a62288507c9be8764e42ac2e63eebc110f
MouchenHung <Mouchen.Hung@quantatw.com>
2024-03-20 13:40:21 +0800

    fby35-ji-Fix_adc_reading_issue

--- meta-facebook/yv35-ji/src/platform/plat_init.c
+++ meta-facebook/yv35-ji/src/platform/plat_init.c
@@ -37,9 +37,9 @@ SCU_CFG scu_cfg[] = {
 	/* Set GPIOM/N/O/P internal pull-up/down after gpio init */
 	{ 0x7e6e261c, 0x0000003A },
 	/* Set GPIOQ/R/S/T internal pull-up/down after gpio init */
-	{ 0x7e6e2630, 0x00000000 },
+	{ 0x7e6e2630, 0xFF000000 },
 	/* Set GPIOU/V/X internal pull-up/down after gpio init */
-	{ 0x7e6e2634, 0x00000000 },
+	{ 0x7e6e2634, 0x000000FF },
 };
 
 void pal_pre_init()
