5f21316f296f8e5bd070b8f63b0059efe165ae5b
MouchenHung <Mouchen.Hung@quantatw.com>
2024-08-26 14:53:39 +0800

    ADD TEST CASE for 1000byte pldm

--- common/shell/commands/info_shell.c
+++ common/shell/commands/info_shell.c
@@ -17,6 +17,9 @@
 #include "info_shell.h"
 #include "plat_version.h"
 #include "util_sys.h"
+#include "plat_mctp.h"
+#include "mctp_ctrl.h"
+#include "stdlib.h"
 
 #ifndef CONFIG_BOARD
 #define CONFIG_BOARD "unknown"
@@ -46,5 +49,99 @@ int cmd_info_print(const struct shell *shell, size_t argc, char **argv)
 	shell_print(
 		shell,
 		"========================{SHELL COMMAND INFO}========================================");
+#if 0
+	mctp *mctp_inst = NULL;
+	mctp_ctrl_msg msg = { 0 };
+
+	if (get_mctp_info_by_eid(0xf0, &mctp_inst, &msg.ext_params) == false) {
+		shell_error(shell, "Failed to get mctp info by eid 0x%x", 0xf0);
+		return 0;
+	}
+
+	struct _set_eid_req set_eid_req = { 0 };
+	struct _set_eid_resp set_eid_resp = { 0 };
+	set_eid_req.op = SET_EID_REQ_OP_SET_EID;
+	set_eid_req.eid = 0xf0;
+
+	msg.hdr.cmd = MCTP_CTRL_CMD_SET_ENDPOINT_ID;
+	msg.hdr.rq = MCTP_REQUEST;
+	msg.cmd_data = (uint8_t *)&set_eid_req;
+	msg.cmd_data_len = sizeof(set_eid_req);
+
+	uint8_t ret = mctp_ctrl_read(mctp_inst, &msg, (uint8_t *)&set_eid_resp,
+					sizeof(set_eid_resp));
+	if (ret != MCTP_SUCCESS) {
+		shell_error(shell, "Fail to set eid to SatMC");
+		return 0;
+	}
+
+	if (set_eid_resp.completion_code != MCTP_SUCCESS) {
+		shell_error(shell, "Fail to set eid to SatMC, completion code: 0x%x",
+			set_eid_resp.completion_code);
+		return 0;
+	}
+
+	shell_print(shell, "Set EID to SatMC successfully");
+#endif
+
+#if 0
+	static uint16_t ii = 1000;
+	uint8_t mctp_dest_eid = 0x08;
+	uint8_t pldm_type = 0x3f;
+	uint8_t pldm_cmd = 0x01;
+
+	uint8_t *req_p = (uint8_t *)malloc(ii * sizeof(uint8_t));
+	if (req_p == NULL) {
+		shell_error(shell, "Failed to allocate memory");
+		return 0;
+	}
+
+	memset(req_p, 0, ii);
+
+	req_p[0] = 0x15;
+	req_p[1] = 0xa0;
+	req_p[2] = 0x00;
+	req_p[3] = 0x18;
+	req_p[4] = 0x01;
+
+	shell_print(shell, "~~ data len %d", ii);
+
+	uint8_t resp_buf[125] = { 0 };
+	pldm_msg pmsg = { 0 };
+	pmsg.hdr.msg_type = MCTP_MSG_TYPE_PLDM;
+	pmsg.hdr.pldm_type = pldm_type;
+	pmsg.hdr.cmd = pldm_cmd;
+	pmsg.hdr.rq = PLDM_REQUEST;
+	pmsg.len = ii;
+	pmsg.buf = req_p;
+
+	mctp *mctp_inst = NULL;
+	if (get_mctp_info_by_eid(mctp_dest_eid, &mctp_inst, &pmsg.ext_params) == false) {
+		shell_error(shell, "Failed to get mctp info by eid 0x%x", mctp_dest_eid);
+		goto exit;
+	}
+
+	printf("mctp_inst: %p\n", mctp_inst);
+	uint16_t resp_len = mctp_pldm_read(mctp_inst, &pmsg, resp_buf, sizeof(resp_buf));
+	if (resp_len == 0) {
+		shell_error(shell, "Failed to get mctp response");
+		goto exit;
+	}
+
+	shell_print(shell, "* mctp: 0x%x addr: 0x%x eid: 0x%x msg_type: 0x%x", mctp_inst,
+		    pmsg.ext_params.smbus_ext_params.addr, mctp_dest_eid, MCTP_MSG_TYPE_PLDM);
+	shell_print(shell, "  pldm_type: 0x%x pldm cmd: 0x%x", pldm_type, pldm_cmd);
+	shell_hexdump(shell, pmsg.buf, pmsg.len);
+
+	if (resp_buf[0] != PLDM_SUCCESS)
+		shell_error(shell, "Response with bad cc 0x%x", resp_buf[0]);
+	else {
+		shell_hexdump(shell, resp_buf, resp_len);
+		shell_print(shell, "");
+	}
+	ii+=1;
+exit:
+	free(req_p);
+#endif
 	return 0;
 }
